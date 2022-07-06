15e1093229b8c5053fc4b71ea844cd7d2af6b90e
Martin Dewe <pesanilham@gmail.com>
2022-07-06 21:40:34 +0700

    Order last

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:perawat_app/NavPages/menu.dart';
 import 'package:perawat_app/SubMenu/invoice.dart';
+import 'package:perawat_app/SubMenu/loading.dart';
+import 'package:perawat_app/SubMenu/orderdone.dart';
 import 'package:perawat_app/home.dart';
 import 'package:perawat_app/splashscreen.dart';
 
@@ -9,7 +11,7 @@ void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MaterialApp(
-   home: Invoice(),
+   home: Splash(),
    
  ));
 }
